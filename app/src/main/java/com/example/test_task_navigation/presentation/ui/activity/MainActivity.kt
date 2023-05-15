23605796a83f92ba4b513f4b4479bca07e19a412
th3caucasian <alan-erkenov@mail.ru>
2023-05-15 22:35:59 +0300

    Это я с Артёмом сделал первую вебку (веб камера)

---
+++ app/src/main/java/com/example/test_task_navigation/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.test_task_navigation.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.test_task_navigation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+
+}
---
+++ app/src/main/java/com/example/test_task_navigation/presentation/ui/fragments/SecondFragment.kt
@@ -0,0 +1,29 @@
+package com.example.test_task_navigation.presentation.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.test_task_navigation.R
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.previous_button)
+        button.setOnClickListener{
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
+}
